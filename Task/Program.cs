8f3c37cdcd107101d4316f2935bbc4436378fac1
Asya <nastya.Z1998@yandex.ru>
2021-12-29 11:02:03 +0500

    added fill array info

---
+++ Task/Program.cs
@@ -0,0 +1,11 @@
+﻿void FillArray(int[] array)
+{
+    int length = array.Length;
+    for (int i = 0; i < length; i++)
+    {
+        array[i] = new Random().Next(-10,10);
+    }
+}
+
+int[] array = new int[5];
+FillArray(array);
