1d642a4899062894e21f2a56053bfa32241f687c
Asya <nastya.Z1998@yandex.ru>
2021-12-29 11:33:44 +0500

    added create new array info

--- Task/Program.cs
+++ Task/Program.cs
@@ -3,7 +3,7 @@
     int length = array.Length;
     for (int i = 0; i < length; i++)
     {
-        array[i] = new Random().Next(-10,10);
+        array[i] = new Random().Next(-10, 10);
     }
 }
 
@@ -18,6 +18,23 @@ string PrintArray(int[] array)
     return result;
 }
 
+string CreateNewArray(int[] array)
+{
+    string res = string.Empty;
+    int length = array.Length;
+    for (int i = 0; i < length; i++)
+    {
+        if (array[i] % 2 == 0)
+        {
+            res += $"{array[i]} ";
+        }
+    }
+    return res;
+}
+
+
 int[] array = new int[5];
+int[] newArray = new int[5];
 FillArray(array);
 Console.WriteLine(PrintArray(array));
+Console.WriteLine(CreateNewArray(array));
