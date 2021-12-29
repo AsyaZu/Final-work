51bc8f9af2986e1905fde259108e400603f9567a
Asya <nastya.Z1998@yandex.ru>
2021-12-29 11:09:26 +0500

    added print array info

--- Task/Program.cs
+++ Task/Program.cs
@@ -7,5 +7,17 @@
     }
 }
 
+string PrintArray(int[] array)
+{
+    string result = string.Empty;
+    int length = array.Length;
+    for (int i = 0; i < length; i++)
+    {
+        result += $"{array[i]} ";
+    }
+    return result;
+}
+
 int[] array = new int[5];
 FillArray(array);
+Console.WriteLine(PrintArray(array));
